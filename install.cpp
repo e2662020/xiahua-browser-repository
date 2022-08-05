310d757b9f4827b1d938d1e03f62923d862ac65a
BuildTools <unconfigured@null.spigotmc.org>
2022-08-05 19:19:46 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .DS_Store
    	new file:   .vscode/settings.json
    	modified:   a.out
    	modified:   install
    	modified:   install.cpp
    	modified:   install.dSYM/Contents/Resources/DWARF/install

--- install.cpp
+++ install.cpp
@@ -1,10 +1,17 @@
+#include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+using namespace std;
+
 int main(void){
+    // 安装依赖库
     system("pip install PyQt5 -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com");
     system("pip install QtWidgets -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com");
     system("pip install QtGui -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com");
     system("pip install QtWebEngineWidgets -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com");
     system("python3 -m pip install QtWebEngineWidgets");
     system("pip --trusted-host pypi.python.org install QtWebEngineWidgets");
+    // 提示用户
+    cout << "安装应该完成了";
+    return 0;
 }
